179f0a0b61db5884a337a1f24092c56923c5c890
Reyad02 <almomenreyad@gmail.com>
2023-07-04 20:27:10 +0600

    folder structure modified

--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="styles/header.css">
   <link rel="stylesheet" href="styles/home.css">
+  <!-- <link rel="stylesheet" href="styles/admin_style.css"> -->
+
   <title>RentEasy</title>
 </head>
 
