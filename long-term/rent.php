44378f7276dd28a9e9034fcc0eb9ea0b7a11612b
Sonjoy Dey <95380764+sonjoy2074@users.noreply.github.com>
2023-07-04 21:04:57 +0600

    new rent page

--- long-term/rent.php
+++ long-term/rent.php
@@ -1,13 +1,39 @@
 <?php
-include "../landingPage/header.php"
+include "../partials/header.php";
 ?>
-<head>
-    <link rel="stylesheet" href="/long-term/rent.css">
-</head>
-<body>
     <div class="row">
+        <div class="col car-info">
+            <img src="/assets/images/landpg/Service1.png" alt="" srcset="">
+            <div class="info">
+                <h4> Key Specifications of BMW X5 </h4>
+                <p> ARAI Mileage 13.38 kmpl <br>
+                    Fuel Type Diesel <br>
+                    Engine Displacement (cc) 2993 <br>
+                    No. of cylinder 6 <br>
+                    Max Power (bhp@rpm) 261.50bhp@4000rpm <br>
+                    Max Torque (nm@rpm) 620Nm@1500-2500rpm <br>
+                    Seating Capacity 5 <br>
+                    TransmissionType Automatic <br>
+                    Fuel Tank Capacity 80.0 <br>
+                    Body Type SUV <br>
+                </p>
+            </div>
+        </div>
         <div class="col">
-            <img src="../landingPage/img/Service2.png" alt="" srcset="">
+            <div class="row">
+                 <div class="col">
+                     <label for="">Pickup Time</label>
+                    <input type="text" placeholder="location">
+                    <label for="">Destination</label>
+                    <input type="text" placeholder="location">
+                 </div>
+                 <div class="col">
+            
+                 </div>
+
+            </div>
         </div>
     </div>
-</body>
+    <?php
+      include "../partials/footer.php"
+    ?>
